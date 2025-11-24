db117a341edc6e8c42884b6b8aaca91ceab4403c
jiseoung <misojskim@naver.com>
2025-11-24 16:41:27 +0900

    fix: verify input values

--- controllers/auth/login.controller.js
+++ controllers/auth/login.controller.js
@@ -12,6 +12,14 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const { id, pw } = req.body;
 
+    if (!id || typeof id !== 'string') {
+        return res.status(400).send('<script>alert("id를 다시 입력해주세요.");location.href="/auth/register"</script>');
+    }
+
+    if (!pw || typeof pw !== 'string') {
+        return res.status(400).send('<script>alert("pw를 다시 입력해주세요.");location.href="/auth/register"</script>');
+    }
+
     const result = await login(id, pw);
 
     if (result.success) {
@@ -23,16 +31,16 @@ router.post('/login', async (req, res) => {
             maxAge: 60 * 60 * 1000,
         });
 
-        return res.status(200).send('<script>alert("Login Success");location.href="/ai/chat"</script>');
+        return res.status(200).send('<script>alert("로그인 성공");location.href="/ai/chat"</script>');
     }
     else {
-        return res.status(400).send('<script>alert("Login Failed");location.href="/auth/login"</script>');
+        return res.status(400).send('<script>alert("로그인 실패");location.href="/auth/login"</script>');
     }
 })
 
 router.post('/logout', (req, res) => {
     res.clearCookie('token');
-    return res.status(200).send('<script>alert("Logout Success");location.href="/"</script>');
+    return res.status(200).send('<script>alert("로그아웃 되었습니다");location.href="/"</script>');
 });
 
 module.exports = router;
